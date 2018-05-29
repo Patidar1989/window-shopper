706bb25557a4f3b8c02b1559bcf072c5f44f1245
Pappulal Patidar <patidar@Pappulals-MacBook-Air.local>
2018-05-29 23:52:04 +0800

    Net nahi chal raha

---
+++ window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Pappulal Patidar on 29/05/2018.
+//  Copyright © 2018 Pappulal Patidar. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
