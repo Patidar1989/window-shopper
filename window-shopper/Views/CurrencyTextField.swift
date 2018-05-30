967d66769e9fd21e864c36ec77abb85f5d86ee78
Pappulal Patidar <patidar@Pappulals-MacBook-Air.local>
2018-05-30 22:20:10 +0800

    Program Button

--- window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/Views/CurrencyTextField.swift
@@ -8,14 +8,29 @@
 
 import UIKit
 
-class CurrencyTextField: UIButton {
+//@IBDesignable - To show the immediate efftect of UI extended class on interface.
+@IBDesignable
+class CurrencyTextField: UITextField {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func prepareForInterfaceBuilder() {
+        customizeView()
     }
-    */
-
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView(){
+        backgroundColor = #colorLiteral(red: 0.7957618833, green: 0.8416960835, blue: 0.9481154084, alpha: 0.3103060788)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes:[.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            
+        }
+    }
 }
